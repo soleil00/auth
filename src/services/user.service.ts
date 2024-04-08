a8793533333cd093432abd30a9df663e7c49e003
soleil00 <srukundo01@gmail.com>
2024-04-08 13:19:14 +0200

    refactored code to remove unused fn

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,8 +1,7 @@
-import { Response,Request } from "express";
+import { Response, Request } from "express";
 import passport from "passport";
 import User from "../Database/models/user";
 import cloudinary from "../config/cloudinary";
-
 
 export const authenticateUser = passport.authenticate("google", {
   scope: ["email", "profile"],
@@ -13,8 +12,6 @@ export const callbackFn = passport.authenticate("google", {
   failureRedirect: "/auth/google/failure",
 });
 
-const handleSuccess = async (req: Request, res: Response) => {};
-
 export const handleUpdate = async (req: any, res: Response) => {
   const { id } = req.params;
   const { first_name, last_name, full_name } = req.body;
@@ -22,17 +19,17 @@ export const handleUpdate = async (req: any, res: Response) => {
     const foundUser = await User.findOne({
       where: { google_id: id },
     });
-    const result = await cloudinary.uploader.upload(req.file.path)
-    if(req.file){
+    const result = await cloudinary.uploader.upload(req.file.path);
+    if (req.file) {
       //@ts-ignore
-       foundUser!.profile = result.secure_url
+      foundUser!.profile = result.secure_url;
     }
     //@ts-ignore
-   first_name ? foundUser.first_name =  first_name:foundUser
+    first_name ? (foundUser.first_name = first_name) : foundUser;
     //@ts-ignore
-   last_name ? foundUser.last_name = last_name:foundUser
+    last_name ? (foundUser.last_name = last_name) : foundUser;
     //@ts-ignore
-   full_name ? foundUser.full_name = full_name:foundUser
+    full_name ? (foundUser.full_name = full_name) : foundUser;
     await foundUser!.save();
     res.status(200).json({
       message: "User information updated successfully",
