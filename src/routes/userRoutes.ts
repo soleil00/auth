b90f8b9e6e7570913daa56ca320725b8822063ee
sabinn12 <irakizasabin12@gmail.com>
2024-04-08 12:20:11 +0200

    added routes for updating user profile and  middlleware for checking if the user is trying to update the email then reject the request

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -7,6 +7,10 @@ import {
   handleFailure,
   handleSuccess,
 } from "../controllers/userControllers";
+import { isLoggedIn } from "../middlewares/isLoggedIn";
+import { isUserFound } from "../middlewares/isFound";
+import { hasPermission } from "../middlewares/hasPermission";
+import { emailValidation } from "../middlewares/emailValidation";
 
 const userRoutes = Router();
 
@@ -15,5 +19,6 @@ userRoutes.get("/auth/google/callback", userService.callbackFn);
 userRoutes.get("/auth/google/success", isAuthenticated, handleSuccess);
 userRoutes.get("/auth/google/failure", handleFailure);
 userRoutes.get("/api/users", getAllUser);
+userRoutes.put("/api/users/profile/:id",isLoggedIn,isUserFound,hasPermission,emailValidation, userService.handleUpdate)
 
 export default userRoutes;
