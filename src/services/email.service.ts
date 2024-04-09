8db3840eb6cfdfd9429ef06c4ac7a11042246ce4
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2024-04-09 15:27:48 +0200

    creating email service

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,31 +1,25 @@
-const nodemailer= require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  host:"smtp-relay.brevo.com",
-  port:587,
-  auth:{
-    user:"emmanuelirumva1@gmail.com",
-    pass:"xsmtpsib-117c79d312a8256eb845beb3d8112f28dab9766dbf3c79f973c9f3c94f682438-qfzK5xNhtgAs8ULS"
-  }
-})
-
-const sendEmail = async (email) =>{
-  const mailOptions = {
-    from: "emmanuelirumva1@gmail.com",
-    to: email,
-    subject:"User Profile Update",
-    text:"Well, your profile updated successfully",
-
-  }
-  try {
-    const info = await transporter.sendMail(mailOptions)
-    console.log(info.response)
-    
-  } catch (error) {
-    console.log(error)
-    
-  }
-  
+import nodemailer from 'nodemailer';
+export const sendEmailToUser = async (email: string) => {
+    const transporter = nodemailer.createTransport({
+        //@ts-ignore
+        host:"smtp-relay.brevo.com",
+        port:587,
+        auth:{
+          user:"emmanuelirumva1@gmail.com",
+          pass:"xsmtpsib-117c79d312a8256eb845beb3d8112f28dab9766dbf3c79f973c9f3c94f682438-qfzK5xNhtgAs8ULS"
+        }
+   })
+    const mailOptions  = {
+        from: process.env.SMTP_EMAIL,
+        to: email,
+        subject:"Updated Email",
+        text: "Well Your profile Updated Successfully"
+    }
+    try {
+        const info = await transporter.sendMail(mailOptions)
+        // console.log('Message sent: %s', info.response);
+    } catch (error) {
+    }
 }
-
-module.exports = sendEmail
+export default sendEmailToUser;
+// module.exports = {sendEmailToUser}
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,7 +2,7 @@ import { Response, Request } from "express";
 import passport from "passport";
 import User from "../Database/models/user";
 import cloudinary from "../config/cloudinary";
-
+import sendEmailToUser from "./email.service";
 export const authenticateUser = passport.authenticate("google", {
   scope: ["email", "profile"],
 });
@@ -30,16 +30,18 @@ export const handleUpdate = async (req: any, res: Response) => {
     last_name ? (foundUser.last_name = last_name) : foundUser;
     //@ts-ignore
     full_name ? (foundUser.full_name = full_name) : foundUser;
-    
+
     await foundUser!.save();
+    //@ts-ignore
+    await sendEmailToUser(foundUser?.email)
     res.status(200).json({
       message: "User information updated successfully",
       user: foundUser,
     });
-  } catch (error) {
+  } catch (error:any) {
     console.error("Error updating user:", error);
     res.status(500).json({
-      message: "Internal server error",
+      message: error.message
     });
   }
 };
